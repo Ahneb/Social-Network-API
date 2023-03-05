e6f61080b77bbbc6a1aa96c2171797ed5290a8d6
Ahneb <benjaminha1996@gmail.com>
2023-03-05 06:34:51 -0800

    set up skeleton for server w/ mongodb conenction set up models based on required items

---
+++ models/Thought.js
@@ -0,0 +1,58 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min_length: 1,
+      max_length: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
